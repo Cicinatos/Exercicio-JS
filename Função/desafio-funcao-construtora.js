a759d2cad1ac57f669d3c67b9011a2754e23554c
Cicinatos <cicinatos@gmail.com>
2020-06-24 20:42:06 -0300

    Atualização e novos exercicios

--- Função/desafio-funcao-construtora.js
+++ Função/desafio-funcao-construtora.js
@@ -1,12 +1,9 @@
-class Pessoa {
-    constructor(nome) {
-        this.nome = nome
-    }
+function Pessoa(nome) {
+    this.nome = nome
 
-    falar() {
+    this.falar = function() {
         console.log(`Meu nome é ${this.nome}`)
     }
 }
-
 const p1 = new Pessoa('João')
 p1.falar()
