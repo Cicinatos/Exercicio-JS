87d152799dcffc8db7fcc544c423ee8280d2325d
Cicinatos <cicinatos@gmail.com>
2020-07-08 21:32:08 -0300

    Teste Atualizado

--- Função/Teste.js
+++ Função/Teste.js
@@ -587,3 +587,59 @@ acharPares()
 
 //27) Construa uma função que receba como parâmetros as alturas e as taxas de crescimento anuais de duas crianças e calcule se existe uma criança menor, caso exista, se a criança menor ultrapassará a maior e em quantos anos isso acontecerá. Utilize centímetro para as unidades de medida.
 
+function calcularCrescimento(altura1, taxa1, altura2, taxa2) {
+    if (altura1 == altura2) {
+        if (taxa1 > taxa2) {
+            return 'A criança 1 ultrapassará a criança 2 em 1 ano.'
+        } else if (taxa1 < taxa2) {
+            return 'A criança 2 ultrapassará a criança 1 em 1 ano.'
+        } else {
+            return 'As crianças tem igual altura de crescimento.'
+        }
+    } else {
+        if (altura1 > altura2) {
+            if (taxa1 >= taxa2) {
+                return 'A criança menor não ultrapassará a criança maior'
+            } else {
+                return calcularTempo(altura2, taxa2, altura1, taxa1)
+            }
+        } else {
+            if (taxa2 >= taxa1) {
+                return 'A criança menor não ultrapassará a criança maior'
+            } else {
+                return calcularTempo(altura1, taxa1, altura2, taxa2)
+            }
+        }
+    }
+}
+
+function calcularTempo(alturaMenor, taxaAlturaMenor, alturaMaior, taxaAlturaMaior) {
+    let qtdAnos = 0
+    while (alturaMenor < alturaMaior) {
+        alturaMenor += taxaAlturaMenor
+        alturaMaior += taxaAlturaMaior
+        qtdAnos++
+    }
+    console.log(qtdAnos)
+    return qtdAnos
+}
+
+console.log(calcularCrescimento(150, 2, 130, 4))
+
+//28) Ler um vetor de números inteiros e imprimir quantos são pares e quantos são impares
+
+function inteiros(numeros) {
+    let qtdPares = 0
+    let qtdImpar = 0
+    for (let i = 0; i < numeros.length; i++) {
+        if (numeros[i] % 2 == 0) {
+            qtdPares++
+        } else {
+            qtdImpar++
+        }
+    }
+    return `${qtdPares} números Pares e ${qtdImpar} números Inpares`
+}
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log(inteiros(numeros))
